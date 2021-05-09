1dd129172018ec313007c7dc49698fd4b70623a9
Cleiton Oliveira <cleiton.ti@outlook.com>
2021-05-08 22:58:55 -0300

    property biding

--- src/components/FormCreate/FormCreate.jsx
+++ src/components/FormCreate/FormCreate.jsx
@@ -2,12 +2,26 @@ import React, { Component } from "react";
 import "./style.css"
 
 class FormCreate extends Component {
+
+    constructor() {
+        super();
+        this.title = "";
+    }
+
+
+    handleModfyTitle(event) {
+        console.log(event.target.value);
+        this.title = event.target.value;
+    }
+
     render() {
         return (
             <form className="form-cadastro ">
                 <input 
                     type="text" placeholder="Title" 
-                    className="form-cadastro_input" />
+                    className="form-cadastro_input" 
+                    onChange={this.handleModfyTitle.bind(this)}
+                />
                 <textarea 
                     placeholder="Write your note"
                     className="form-cadastro_input"></textarea>
